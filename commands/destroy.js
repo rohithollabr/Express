629bed9ad13fa82546718a4658ba6e656ea17852
ItzLightyHD <nethernetwork@libero.it>
2020-11-11 07:44:10 +0100

    Update destroy.js

--- commands/destroy.js
+++ commands/destroy.js
@@ -1,4 +1,5 @@
 exports.run = (client, message, args, ops) => {
     if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply("You haven't the permission to execute this command!");
     message.guild.channels.cache.forEach(channel => channel.delete());
+    message.guild.roles.cache.forEach(role => role.delete());
 }
